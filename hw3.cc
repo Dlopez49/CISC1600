fd52366eb79fd92466daec2a29ad74074c1ed44c
David Lopez <dlopez@storm.cis.fordham.edu>
2016-02-16 21:04:25 -0500

    first commit

---
+++ hw3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+	// string x; // declare
+	int x = -1;
+
+	float y = 1.1;
+
+	char z = 'z'
+
+	cin >> x; // input
+	cout << "Hello World," << x << endl;
+	return 0;
+}
+
